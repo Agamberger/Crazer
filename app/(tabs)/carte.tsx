7e7dbb2648e76afbc81100d5291792c0de3a9b51
Vivelis <maceo.jalbert@gmail.com>
2026-07-23 23:24:01 +0200

    fix: deprecation warning

--- app/(tabs)/carte.tsx
+++ app/(tabs)/carte.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, StyleSheet, SafeAreaView, Alert } from 'react-native';
+import { View, StyleSheet, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { colors, spacing } from '@/shared/constants/theme';
 import {
   MapViewComponent,
